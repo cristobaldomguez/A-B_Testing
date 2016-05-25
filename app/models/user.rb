dc5608f767c26b9c65cbfa15a857017c943f1d64
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-05-25 19:07:55 -0400

    Users method for defauslt user and values

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ActiveRecord::Base
 
 
          enum role: [:admin, :user, :guest]
+
+  def default_role
+    self.role ||= 2
+  end
+  
+  def default_values
+  	self.role ||= 0
+  end
+  
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+  
 end
