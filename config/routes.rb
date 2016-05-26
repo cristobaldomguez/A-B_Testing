5a50532dd0f36d97f3b3b01defa63fbcf7d9cd17
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-05-26 16:58:27 -0400

    Re-Creating Ab_test, Sites & Users controllers and views with RESTful actions: new, create, update, edit, destroy, index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'sites/new'
+
+  get 'sites/create'
+
+  get 'sites/update'
+
+  get 'sites/edit'
+
+  get 'sites/destroy'
+
+  get 'sites/index'
+
+  get 'sites/show'
+
+  get 'ab_tests/new'
+
+  get 'ab_tests/create'
+
+  get 'ab_tests/update'
+
+  get 'ab_tests/edit'
+
+  get 'ab_tests/destroy'
+
+  get 'ab_tests/index'
+
+  get 'ab_tests/show'
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
       resources :sites
