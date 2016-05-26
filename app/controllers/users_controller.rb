66a613bcf1e751c5e73a150de528afae92036fc6
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-05-26 18:02:34 -0400

    Adding some fields like firstname and lastname for user on schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,85 @@
 class UsersController < ApplicationController
+ class UsersController < ApplicationController
+  load_and_authorize_resource
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  layout "settings"
+  @access_to_section = ["groups"]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.all
+  end
+
+  def new
+    @groups = Group.all
+     @user = User.new 
+     @u = User.select(:role)
+  end
+
+  def edit
+    @groups = Group.all
+     @user = User.all
+  end
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @users }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  # PATCH/PUT /products/1.json
+  def update
+    respond_to do |format|
+      if @user.update(product_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:serialuser, :username, :role, :name, :email)
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   def new
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507003940) do
+ActiveRecord::Schema.define(version: 20160526220109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20160507003940) do
     t.integer  "role"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "lastname"
+    t.string   "firstname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
