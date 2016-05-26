e2ccf841121e6ab9a7e1783cecf0d7deb1b7eb1e
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-05-26 17:40:45 -0400

    Stylesheets, routes & settings for the initial page

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,19 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  devise_for :users
 
-  get 'users/create'
+  	 devise_scope :user do
 
-  get 'users/update'
+        authenticated :user do
+        root 'entry#start', as: :authenticated_root
+        end
 
-  get 'users/edit'
-
-  get 'users/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'sites/new'
-
-  get 'sites/create'
-
-  get 'sites/update'
-
-  get 'sites/edit'
-
-  get 'sites/destroy'
-
-  get 'sites/index'
-
-  get 'sites/show'
-
-  get 'ab_tests/new'
-
-  get 'ab_tests/create'
-
-  get 'ab_tests/update'
-
-  get 'ab_tests/edit'
-
-  get 'ab_tests/destroy'
-
-  get 'ab_tests/index'
-
-  get 'ab_tests/show'
-
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+    
+        unauthenticated do
+        root 'devise/sessions#new', as: :unauthenticated_root
+        end 
+      end 
+ 
 
       resources :sites
       resources :ab_tests
