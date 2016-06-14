5154ddfeb4f7b661c6700dead1b0ea83fba8e976
Cristobal Dominguez <cristobald@me.com>
2016-06-13 22:14:54 -0400

    Adds nested routes and changes logued route

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
 
   devise_scope :user do
 
     authenticated :user do
-      root 'entry#start', as: :authenticated_root
+      root 'sites#index', as: :authenticated_root
     end
-
     
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end 
   end 
-  
 
-  resources :sites
-  resources :ab_tests
+  resources :sites do
+    resources :ab_tests, only: [:index, :show]
+  end
 
   get 'entry/startsettings'
   get 'entry/start'
-
 end
