1757039c838da0c9627672ebf482546d46e84a58
Cristobal Dominguez <cristobald@me.com>
2016-06-15 16:44:29 -0400

    Bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
- class UsersController < ApplicationController
   load_and_authorize_resource
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   layout "settings"
